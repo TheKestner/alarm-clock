8f6fd7909ad5335a2d427ce695ddf5b049882071
Zachary Kestner <zac.kestner@gmail.com>
2022-02-11 07:02:18 +0000

    fixed typo from js change

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let clock = document.getElementById("clock");
 
 let today = new Date();
-let date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear;
+let date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
 let time = today.getHours() + ":"+ today.getMinutes() + ":"+ today.getSeconds();
 let dateTime = date+' '+time;
 clock.innerHTML = dateTime;
