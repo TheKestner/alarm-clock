17bd0e9b2cbe562b4f6f09b3f5c8484aa2c942f1
Zachary Kestner <zac.kestner@gmail.com>
2022-02-11 06:12:57 +0000

    Javascript changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+let today = new Date();
+let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+let time = today.getHours() + ":"+ today.getMinutes(); + ":" + today.getSeconds();
+let dateTime = date+' '+time;
+let clock = document.getElementById("clock");
+clock.innerHTML = dateTime;
+
 setInterval(updatingTime, 1000)
 
 function updatingTime(){
