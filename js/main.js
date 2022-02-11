528bcabf9f0de448006fbd2ba3fd8d33885f50b0
Zachary Kestner <zac.kestner@gmail.com>
2022-02-11 08:25:02 +0000

    added alert

--- js/main.js
+++ js/main.js
@@ -6,10 +6,18 @@ let time = today.getHours() + ":"+ today.getMinutes() + ":"+ today.getSeconds();
 let dateTime = date+' '+time;
 clock.innerHTML = dateTime;
 
-setInterval(updatingTime, 1000)
-
 function updatingTime(){
+    let today = new Date();
+    let date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
+    let time = today.getHours() + ":"+ today.getMinutes() + ":"+ today.getSeconds();
+    let dateTime = date+' '+time;
+    clock.innerHTML = dateTime;
+
+    if (today.getHours() > 5) {
+        alert("Time for Stand up!")
+    }
 
 }
 
+setInterval(updatingTime, 1000)
 
